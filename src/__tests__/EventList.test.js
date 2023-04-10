9649eb7750d3d22e834b400b0ec012e033402b5c
GabCB <gabycuencas@gmail.com>
2023-04-10 21:54:21 +0200

    create EventList.test.js (test cases task 4.4)

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import EventList from '../EventList';
+import Event from '../Event'; //in src/__tests__/EventList.test.js
+import { mockData } from '../mock-data';
+
+describe('<EventList /> component', () => {
+  test('render correct number of events', () => {
+    const EventListWrapper = shallow(<EventList events= {mockData} />);
+    expect (EventListWrapper.find(Event)).toHaveLength(mockData.length);
+  });
+
+});
