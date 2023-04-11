f6d2328cb8a31323acdad276cc67e03b9c543d4f
GabCB <gabycuencas@gmail.com>
2023-04-11 13:05:36 +0200

    styling app UI

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -5,7 +5,8 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
   state = {
     query: '',
-    suggestions: []
+    suggestions: [],
+    showSuggestions: undefined
   }
 
   handleInputChanged = (event) => {
@@ -21,7 +22,8 @@ class CitySearch extends Component {
 
   handleItemClicked = (suggestion) => {
     this.setState({
-      query: suggestion
+      query: suggestion,
+      showSuggestions: false
     });
 
     this.props.updateEvents(suggestion);
@@ -35,8 +37,9 @@ class CitySearch extends Component {
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
-        <ul className="suggestions">
+        <ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
           {this.state.suggestions.map((suggestion) => (
             <li 
             key={suggestion}
