327b72e19022a1512975c3085fe90cfe09574e86
GabCB <gabycuencas@gmail.com>
2023-04-13 14:35:49 +0200

    modifuy NumberofEvents position from render

--- src/App.js
+++ src/App.js
@@ -65,8 +65,8 @@ class App extends Component {
     return (
       <div className="App">
         <CitySearch  locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
       </div>
     );
   }
