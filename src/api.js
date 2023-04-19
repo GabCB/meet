c4be75d3b714d53c91a9191c8b62489903b2f1d4
GabCB <gabycuencas@gmail.com>
2023-04-19 09:50:30 +0200

    update api.js

--- src/api.js
+++ src/api.js
@@ -42,20 +42,17 @@ const removeQuery = () => {
 };
 
 const getToken = async (code) => {
-  try {
-      const encodeCode = encodeURIComponent(code);
-
-      const response = await fetch( 'https://y2jdjhxe84.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
-      if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      const { access_token } = await response.json();
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch (
+    'https://y2jdjhxe84.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+  )
+    .then ((res) => {
+      return res.json();
+    })
+    .catch ((error) => error);
       access_token && localStorage.setItem("access_token", access_token);
       return access_token;
-  } catch(error) {
-      error.json();
-  }
-};
+  };
 
 export const getEvents = async () => {
   NProgress.start();
