383840f2e72440ac311c0076a3afdc54ad32acaf
GabCB <gabycuencas@gmail.com>
2023-04-21 12:14:53 +0200

    update mock-data for data visualization (Task 4.10)

--- src/mock-data.js
+++ src/mock-data.js
@@ -77,6 +77,498 @@ const mockData = [
       },
       "eventType": "default"
      },
+     {
+      "kind": "calendar#event",
+      "etag": "\"3187232047578000\"",
+      "id": "013qoq4ns44cam7q4hqcf13eab_20230317T000000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=MDEzcW9xNG5zNDRjYW03cTRocWNmMTNlYWJfMjAyMzAzMTdUMDAwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-07-01T15:05:49.000Z",
+      "updated": "2020-07-01T15:07:03.789Z",
+      "summary": "jQuery and More",
+      "description": "Do you know jQuery is used by around 70 percent of the 10 million most popular websites as of May 2019? \n\nThough many consider it dead after Angular and Express gained popularity, jQuery is still an important part of many websites. \n\nIn our workshop, we teach basic to advanced jQuery where you will also be able to build a simple app using it. If you are familiar with JS, join us to learn probably its most popular library.",
+      "location": "Santiago, Santiago Metropolitan Region, Chile",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-17T01:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-17T02:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "013qoq4ns44cam7q4hqcf13eab",
+      "originalStartTime": {
+        "dateTime": "2023-03-17T01:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "013qoq4ns44cam7q4hqcf13eab@google.com",
+      "sequence": 1,
+      "hangoutLink": "https://meet.google.com/mqx-jyfg-uoc",
+      "conferenceData": {
+        "entryPoints": [
+          {
+            "entryPointType": "video",
+            "uri": "https://meet.google.com/mqx-jyfg-uoc",
+            "label": "meet.google.com/mqx-jyfg-uoc"
+          },
+          {
+            "entryPointType": "more",
+            "uri": "https://tel.meet/mqx-jyfg-uoc?pin=9759553023326",
+            "pin": "9759553023326"
+          },
+          {
+            "regionCode": "DE",
+            "entryPointType": "phone",
+            "uri": "tel:+49-40-8081618734",
+            "label": "+49 40 8081618734",
+            "pin": "678190650"
+          }
+        ],
+        "conferenceSolution": {
+          "key": {
+            "type": "hangoutsMeet"
+          },
+          "name": "Google Meet",
+          "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+        },
+        "conferenceId": "mqx-jyfg-uoc"
+      },
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    },
+    {
+      "kind": "calendar#event",
+      "etag": "\"3187222416180000\"",
+      "id": "6co0ojl0l0j0rbh3g56vlc2s9i_20230317T020000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=NmNvMG9qbDBsMGowcmJoM2c1NnZsYzJzOWlfMjAyMzAzMTdUMDIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-07-01T13:44:15.000Z",
+      "updated": "2020-07-01T13:46:48.090Z",
+      "summary": "React California",
+      "description": "React is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nReact California is a non-profit organization offering free training sessions to React enthusiasts every Wednesday and Friday.\n\nJoin us in our free React training sessions and give your career a new direction.",
+      "location": "California, USA",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-17T03:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-17T04:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "6co0ojl0l0j0rbh3g56vlc2s9i",
+      "originalStartTime": {
+        "dateTime": "2023-03-17T03:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "6co0ojl0l0j0rbh3g56vlc2s9i@google.com",
+      "sequence": 1,
+      "hangoutLink": "https://meet.google.com/imt-kabh-cib",
+      "conferenceData": {
+        "entryPoints": [
+          {
+            "entryPointType": "video",
+            "uri": "https://meet.google.com/imt-kabh-cib",
+            "label": "meet.google.com/imt-kabh-cib"
+          },
+          {
+            "entryPointType": "more",
+            "uri": "https://tel.meet/imt-kabh-cib?pin=8711708481601",
+            "pin": "8711708481601"
+          },
+          {
+            "regionCode": "DE",
+            "entryPointType": "phone",
+            "uri": "tel:+49-40-8081618688",
+            "label": "+49 40 8081618688",
+            "pin": "752695860"
+          }
+        ],
+        "conferenceSolution": {
+          "key": {
+            "type": "hangoutsMeet"
+          },
+          "name": "Google Meet",
+          "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+        },
+        "conferenceId": "imt-kabh-cib"
+      },
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    },
+    {
+      "kind": "calendar#event",
+      "etag": "\"3187232499498000\"",
+      "id": "7rclpdp8egm60g7kepei7j0bt9_20230317T100000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=N3JjbHBkcDhlZ202MGc3a2VwZWk3ajBidDlfMjAyMzAzMTdUMTAwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-07-01T15:10:03.000Z",
+      "updated": "2020-07-01T15:10:49.749Z",
+      "summary": "Hello JavaScript!!",
+      "description": "You know basic HTML and CSS, and interested to learn the next thing about web development? You are at the right place. Our workshop will take you to the amazing world of JavaScript. \n\nIn this workshop, you'll:- Learn JavaScript, from basics to advanced topics- Create your first app",
+      "location": "Bangkok, Thailand",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-17T11:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-17T12:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "7rclpdp8egm60g7kepei7j0bt9",
+      "originalStartTime": {
+        "dateTime": "2023-03-17T11:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "7rclpdp8egm60g7kepei7j0bt9@google.com",
+      "sequence": 1,
+      "hangoutLink": "https://meet.google.com/tbu-cgma-ikb",
+      "conferenceData": {
+        "entryPoints": [
+          {
+            "entryPointType": "video",
+            "uri": "https://meet.google.com/tbu-cgma-ikb",
+            "label": "meet.google.com/tbu-cgma-ikb"
+          },
+          {
+            "entryPointType": "more",
+            "uri": "https://tel.meet/tbu-cgma-ikb?pin=5464596701700",
+            "pin": "5464596701700"
+          },
+          {
+            "regionCode": "DE",
+            "entryPointType": "phone",
+            "uri": "tel:+49-40-8081618071",
+            "label": "+49 40 8081618071",
+            "pin": "382271360"
+          }
+        ],
+        "conferenceSolution": {
+          "key": {
+            "type": "hangoutsMeet"
+          },
+          "name": "Google Meet",
+          "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+        },
+        "conferenceId": "tbu-cgma-ikb"
+      },
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    },
+    {
+      "kind": "calendar#event",
+      "etag": "\"3181159875584000\"",
+      "id": "3qtd6uscq4tsi6gc7nmmtpqlct_20230317T130000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMzAzMTdUMTMwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-05-19T19:14:30.000Z",
+      "updated": "2020-05-27T11:45:37.792Z",
+      "summary": "React is Fun",
+      "description": "Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ",
+      "location": "Berlin, Germany",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-17T14:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-17T15:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "3qtd6uscq4tsi6gc7nmmtpqlct",
+      "originalStartTime": {
+        "dateTime": "2023-03-17T14:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "3qtd6uscq4tsi6gc7nmmtpqlct@google.com",
+      "sequence": 0,
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    },
+    {
+      "kind": "calendar#event",
+      "etag": "\"3187234500432000\"",
+      "id": "65jg7h2f96klim15fie75mj1a1_20230317T150000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=NjVqZzdoMmY5NmtsaW0xNWZpZTc1bWoxYTFfMjAyMzAzMTdUMTUwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-07-01T15:26:57.000Z",
+      "updated": "2020-07-01T15:27:30.216Z",
+      "summary": "AngularJS Workshop",
+      "description": "Developed by Google, AngularJS is a relatively new JavaScript, and it is designed to make front-end development as easy as possible for you. \n\nJoin us to get introduced to this wonderful framework and dive deep into its features.",
+      "location": "Cape Town, South Africa",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-17T16:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-17T17:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "65jg7h2f96klim15fie75mj1a1",
+      "originalStartTime": {
+        "dateTime": "2023-03-17T16:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "65jg7h2f96klim15fie75mj1a1@google.com",
+      "sequence": 1,
+      "hangoutLink": "https://meet.google.com/ctp-pwtc-pmq",
+      "conferenceData": {
+        "entryPoints": [
+          {
+            "entryPointType": "video",
+            "uri": "https://meet.google.com/ctp-pwtc-pmq",
+            "label": "meet.google.com/ctp-pwtc-pmq"
+          },
+          {
+            "entryPointType": "more",
+            "uri": "https://tel.meet/ctp-pwtc-pmq?pin=2824243883315",
+            "pin": "2824243883315"
+          },
+          {
+            "regionCode": "DE",
+            "entryPointType": "phone",
+            "uri": "tel:+49-30-300195220",
+            "label": "+49 30 300195220",
+            "pin": "987090091"
+          }
+        ],
+        "conferenceSolution": {
+          "key": {
+            "type": "hangoutsMeet"
+          },
+          "name": "Google Meet",
+          "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+        },
+        "conferenceId": "ctp-pwtc-pmq"
+      },
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    },
+    {
+      "kind": "calendar#event",
+      "etag": "\"3187221858184000\"",
+      "id": "3gi3kabkm3bua5lq3jhscc8s4c_20230317T210000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=M2dpM2thYmttM2J1YTVscTNqaHNjYzhzNGNfMjAyMzAzMTdUMjEwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-07-01T13:23:24.000Z",
+      "updated": "2020-07-01T13:42:09.092Z",
+      "summary": "Intro to AngularJS-Remote",
+      "description": "Developed by Google, AngularJS is a relatively new JavaScript, and it is designed to make front-end development as easy as possible for you. Join us to get introduced to this wonderful framework and dive deep into its features. ",
+      "location": "New York, NY, USA",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-17T22:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-17T23:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "3gi3kabkm3bua5lq3jhscc8s4c",
+      "originalStartTime": {
+        "dateTime": "2023-03-17T22:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "3gi3kabkm3bua5lq3jhscc8s4c@google.com",
+      "sequence": 2,
+      "hangoutLink": "https://meet.google.com/xcb-hfjb-psv",
+      "conferenceData": {
+        "entryPoints": [
+          {
+            "entryPointType": "video",
+            "uri": "https://meet.google.com/xcb-hfjb-psv",
+            "label": "meet.google.com/xcb-hfjb-psv"
+          },
+          {
+            "entryPointType": "more",
+            "uri": "https://tel.meet/xcb-hfjb-psv?pin=4442155852117",
+            "pin": "4442155852117"
+          },
+          {
+            "regionCode": "DE",
+            "entryPointType": "phone",
+            "uri": "tel:+49-40-8081617300",
+            "label": "+49 40 8081617300",
+            "pin": "657955248"
+          }
+        ],
+        "conferenceSolution": {
+          "key": {
+            "type": "hangoutsMeet"
+          },
+          "name": "Google Meet",
+          "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+        },
+        "conferenceId": "xcb-hfjb-psv"
+      },
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    },
+    {
+      "kind": "calendar#event",
+      "etag": "\"3187232532588000\"",
+      "id": "20c51je3q99mnolr1rp06dt9jd_20230318T130000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=MjBjNTFqZTNxOTltbm9scjFycDA2ZHQ5amRfMjAyMzAzMThUMTMwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-07-01T14:17:12.000Z",
+      "updated": "2020-07-01T15:11:06.294Z",
+      "summary": "Use jQuery, bring in interactivity easily ",
+      "description": "Do you know jQuery is used by around 70 percent of the 10 million most popular websites as of May 2019? \n\nThough many consider it dead after Angular and Express gained popularity, jQuery is still an important part of many websites. \n\nIn our workshop, we teach basic to advanced jQuery where you will also be able to build a simple app using it. If you are familiar with JS, join us to learn probably its most popular library.",
+      "location": "Mumbai, Maharashtra, India",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-18T14:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-18T15:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "20c51je3q99mnolr1rp06dt9jd",
+      "originalStartTime": {
+        "dateTime": "2023-03-18T14:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "20c51je3q99mnolr1rp06dt9jd@google.com",
+      "sequence": 1,
+      "hangoutLink": "https://meet.google.com/nns-htdr-ijk",
+      "conferenceData": {
+        "entryPoints": [
+          {
+            "entryPointType": "video",
+            "uri": "https://meet.google.com/nns-htdr-ijk",
+            "label": "meet.google.com/nns-htdr-ijk"
+          },
+          {
+            "entryPointType": "more",
+            "uri": "https://tel.meet/nns-htdr-ijk?pin=9302146320475",
+            "pin": "9302146320475"
+          },
+          {
+            "regionCode": "DE",
+            "entryPointType": "phone",
+            "uri": "tel:+49-40-8081618430",
+            "label": "+49 40 8081618430",
+            "pin": "810644618"
+          }
+        ],
+        "conferenceSolution": {
+          "key": {
+            "type": "hangoutsMeet"
+          },
+          "name": "Google Meet",
+          "iconUri": "https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png"
+        },
+        "conferenceId": "nns-htdr-ijk"
+      },
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    },
+    {
+      "kind": "calendar#event",
+      "etag": "\"3181161784712000\"",
+      "id": "4eahs9ghkhrvkld72hogu9ph3e_20230318T150000Z",
+      "status": "confirmed",
+      "htmlLink": "https://www.google.com/calendar/event?eid=NGVhaHM5Z2hraHJ2a2xkNzJob2d1OXBoM2VfMjAyMzAzMThUMTUwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20",
+      "created": "2020-05-19T19:17:46.000Z",
+      "updated": "2020-05-27T12:01:32.356Z",
+      "summary": "Learn JavaScript",
+      "description": "Have you wondered how you can ask Google to show you the list of the top ten must-see places in London? And how Google presents you the list? How can you submit the details of an application? Well, JavaScript is doing these. :) \n\nJavascript offers interactivity to a dull, static website. Come, learn JavaScript with us and make those beautiful websites.",
+      "location": "London, UK",
+      "creator": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "organizer": {
+        "email": "fullstackwebdev@careerfoundry.com",
+        "self": true
+      },
+      "start": {
+        "dateTime": "2023-03-18T16:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "end": {
+        "dateTime": "2023-03-18T17:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "recurringEventId": "4eahs9ghkhrvkld72hogu9ph3e",
+      "originalStartTime": {
+        "dateTime": "2023-03-18T16:00:00+01:00",
+        "timeZone": "Europe/Berlin"
+      },
+      "iCalUID": "4eahs9ghkhrvkld72hogu9ph3e@google.com",
+      "sequence": 0,
+      "reminders": {
+        "useDefault": true
+      },
+      "eventType": "default"
+    }
 
 ];
 
