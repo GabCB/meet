44a73eb7215d26b11de5c53379d35bf5c19ea88f
GabCB <gabycuencas@gmail.com>
2023-04-24 11:53:06 +0200

    update color

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ class App extends Component {
               <XAxis type="category" dataKey="city" name="city" />
               <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
               <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-              <Scatter data={this.getData()} fill="#e7acb5" />
+              <Scatter data={this.getData()} fill="#c99797" />
             </ScatterChart>
           </ResponsiveContainer>
         </div>
